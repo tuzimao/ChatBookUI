1d29e2c2925f348e52961fe70b4b5e0a2022e17b
tuzimao <zliu1@ualberta.ca>
2024-01-26 18:26:38 -0800

    update with post method

--- src/pages/api/settemplate.ts
+++ src/pages/api/settemplate.ts
@@ -1,9 +1,13 @@
 // @ts-ignore
-import { NextApiRequest, NextApiResponse } from 'next';
-import { setTemplate } from '../../utils/utils';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { setTemplate } from '../../utils/utils'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const setTemplateData: any = await setTemplate(req.body);
-    
-    res.status(200).json(setTemplateData);
+  if (req.method === 'POST') {
+    const setTemplateData: any = await setTemplate(req.body)
+    res.status(200).json(setTemplateData)
+  } else {
+    // 如果请求方法不是POST，返回405方法不允许
+    res.status(405).end('Method Not Allowed')
+  }
 }
